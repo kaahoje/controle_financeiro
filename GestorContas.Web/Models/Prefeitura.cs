cfad59f48fc0e263c84481b5e3101b0f71cc37cd
José Adailton dos Santos <adailton.system@hotmail.com>
2026-02-21 12:01:13 -0300

    Ajustes no componente que indica o recebimento/pagamento.

--- GestorContas.Web/Models/Prefeitura.cs
+++ GestorContas.Web/Models/Prefeitura.cs
@@ -8,15 +8,20 @@ namespace GestorContas.Web.Models
         [Key]
         public int Id { get; set; }
 
+        [Required(ErrorMessage = "A descrição é obrigatória")]
         [Display(Name = "Descrição")]
         public string? Descricao { get; set; }
 
         public DateTime? Data { get; set; }
 
+        [Required(ErrorMessage = "O valor é obrigatório")]
+        [Range(0.01, double.MaxValue, ErrorMessage = "O valor deve ser maior que zero")]
         public decimal? Valor { get; set; }
 
-        public bool? Entrada { get; set; }
+        [Display(Name = "É Recebimento?")]
+        public bool Entrada { get; set; }
 
+        [Required(ErrorMessage = "A data de vencimento é obrigatória")]
         [Display(Name = "Vencimento da Parcela")]
         public DateTime? VencimentoDaParcela { get; set; }
     }
